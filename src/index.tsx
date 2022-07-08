7369a6836bce50a83274fbe5e4564a38f4a7a72a
Ikamdeep Singh Grewal <is4grewal@uwaterloo.ca>
2022-07-08 00:07:50 -0400

    -

--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,11 @@ import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import { registerLicense } from '@syncfusion/ej2-base';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
-
+registerLicense('ORg4AjUWIQA/Gnt2VVhiQlFadVlJXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxRdkJjXn1dcXRXQ2ZYUUY=')
 root.render(
   <React.StrictMode>
     <Provider store={store}>
